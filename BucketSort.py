0b0747926bcd1f29713207b2a512d90c83449897
NikitaBukreyev <73642125+NikitaBukreyev@users.noreply.github.com>
2020-12-08 17:51:43 +0200

    Delete BucketSort.py

--- BucketSort.py
+++
@@ -1,8 +0,0 @@
-import time
-
-def bucketSort(array):
-    start = time.perf_counter()
-
-    end = time.perf_counter()
-    finalTime = 1000 * (end - start)
-    return "RadixSort for "+ str(len(array)) + " elements took "+ str(round(finalTime,10))+" miliseconds"
